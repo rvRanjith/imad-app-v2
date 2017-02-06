8a20c4ab010ea02c84672118864f30214035ec25
rvRanjith <ranjithvinayagam@gmail.com>
2017-02-06 19:42:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleon-ONE',function(req,res){
+app.get('/article-one',function(req,res){
  res.send('Article one req will be served here');
 });
 
-app.get('/article-TWO',function(req,res){
+app.get('/article-two',function(req,res){
  res.send('Article TWO req will be served here');
 });
 
-app.get('/article-THREE',function(req,res){
+app.get('/article-three',function(req,res){
  res.send('Article three req will be served here');
 });
 
