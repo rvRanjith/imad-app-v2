756138a689e61d5dfb7f4f78a1aa686904dc3a06
rvRanjith <ranjithvinayagam@gmail.com>
2017-02-06 19:41:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleon-ONE',function(req,res){
+ res.send('Article one req will be served here');
+});
+
+app.get('/article-TWO',function(req,res){
+ res.send('Article TWO req will be served here');
+});
+
+app.get('/article-THREE',function(req,res){
+ res.send('Article three req will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
