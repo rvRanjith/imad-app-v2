343a7db6981c17d7784cb86ccabd52f14f8907e2
rvRanjith <ranjithvinayagam@gmail.com>
2017-02-15 12:01:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ function moveRight ()
 marginLeft = marginLeft + 10;
 img.style.marginLeft = marginLeft + 'px';
 }
-img.onclick = function ()
+img.onclick =function ()
 {
 var interval = setInterval(moveRight, 50);
 };
