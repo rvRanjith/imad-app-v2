f52d3f54cd63200f3561409e8ba90b9988ec3947
rvRanjith <ranjithvinayagam@gmail.com>
2017-02-21 12:42:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,14 @@ var content=data.content;
    `;
   return htmlTemplate ;
 }
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter +1;
+  res.send(counter.tostring());
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
