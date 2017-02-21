34c8b1c64efc37f5f228c450f7c3e201c18bbc6b
rvRanjith <ranjithvinayagam@gmail.com>
2017-02-22 01:06:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,20 @@
-console.log('Loaded!');
-
-//change the text of center text
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-
-//move image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () 
-{
-marginLeft = marginLeft + 5;
-img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick =function ()
-{
-var interval = setInterval(moveRight, 50);
+// counter code
+var button = document.getElementById('counter');
+button.onclick=function(){
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the resource
+    request.onreadystatechange=function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+            // take some action
+            if (request.status === 200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    //make the request
+    request.open('GET','http://rvranjith.imad.hasura-app.io/',true);
+    request.send(null);
 };
